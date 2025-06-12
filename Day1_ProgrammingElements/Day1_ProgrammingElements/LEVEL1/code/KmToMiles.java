f30db9859f79516b5f77c70edffd16e1a9ded50d
Anmoltiwari1 <124855487+Anmoltiwari1@users.noreply.github.com>
2025-06-12 11:55:41 +0530

    Add files via upload
    Commited [Anmol Tiwari]

---
+++ Day1_ProgrammingElements/Day1_ProgrammingElements/LEVEL1/code/KmToMiles.java
@@ -0,0 +1,13 @@
+package code;
+
+public class KmToMiles {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double km = 10.8;
+        double miles = km * 1.6;
+        System.out.println("The distance " + km + " km in miles is " + miles);
+
+	}
+
+}
